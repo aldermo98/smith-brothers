bc81b0a00cc9b5ff280e54d41bbd333ff9ca7b96
morenoal <skinymoreno98@gmail.com>
2025-08-07 00:28:49 -0700

    updated footer links

--- web-components/footer.js
+++ web-components/footer.js
@@ -16,31 +16,18 @@ class CustomFooter extends HTMLElement {
                             <div class="widget-content">
                                 <h4>Company</h4>
                                 <ul class="widget-links">
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Our Services</a></li>
-                                    <li><a href="#">Clients Reviews</a></li>
-                                    <li><a href="#">Contact Us</a></li>
+                                    <li><a href="/about-us.html">About Us</a></li>
+                                    <li><a href="/services.html">Our Services</a></li>
+                                    <li><a href="/gallery.html">Before & Afters</a></li>
+                                    <li><a href="/free-estimate">Free Estimate</a></li>
                                 </ul>
                             </div>
                         </div>
                         <div class="col-lg-4 col-sm-12 sm-padding ml-auto">
                             <div class="widget-content">
                                 <h4>Contact Us</h4>
-                                <div><a href="mailto:info@support.smithbrothersdevelopment.homes"><i class="fa fa-envelope mr-2"></i> info@support.smithbrothersdevelopment.homes</a></div>
+                                <div><a href="mailto:info@support.smithbrothersdevelopment.homes"><i class="fa fa-envelope mr-2"></i> info@smithbrothersdevelopment.homes</a></div>
                                 <div><a href="tel:2532851656"><i class="fa fa-phone mr-2"></i> +1 (253) 285-1656</a></div>
-                                <p>Subscribe and get 10% off from our <br>architecture company.</p>
-                                <div class="subscribe-box clearfix">
-                                    <div class="subscribe-form-wrap">
-                                        <form action="#" class="subscribe-form">
-                                            <input type="email" name="email" id="subs-email" class="form-input" placeholder="Enter Your Email Address...">
-                                            <button type="submit" class="submit-btn">Subscribe</button>
-                                            <div id="subscribe-result">
-                                                <p class="subscription-success"></p>
-                                                <p class="subscription-error"></p>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
